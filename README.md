0c60503b626bfdfdddf69a640f01907b2d5f2bb6
Calin Radoni <CalinRadoni@users.noreply.github.com>
2020-10-07 23:43:42 +0300

    Adds a python generator and jinja2 template for README.md

--- README.md
+++ README.md
@@ -6,22 +6,27 @@ and I like to build things.
 
 Now I am working with ESP32, WS2812* LEDs and ESP-IDF. The current hardware and software projects are:
 
-- [pax-LampD1](https://github.com/CalinRadoni/pax-LampD1)
-- [pax-Gateway](https://github.com/CalinRadoni/pax-Gateway)
+- [pax-LampD1](https://api.github.com/repos/CalinRadoni/pax-LampD1)<br/>*A lamp based on pax-DLED and 2 x 100 WS2812B LED strips*<br/>C++
+- [pax-Gateway](https://api.github.com/repos/CalinRadoni/pax-Gateway)<br/>KiCad Layout
 
 For those I have built some ESP-IDF components like:
 
-- https://github.com/CalinRadoni/ESP32BoardManager
-- https://github.com/CalinRadoni/ESP32RMT
-- https://github.com/CalinRadoni/ESP32Timers
-- https://github.com/CalinRadoni/ESP32HAL
-- https://github.com/CalinRadoni/ESP32SimpleOTA
-- https://github.com/CalinRadoni/ESP32DLEDController
+- [ESP32BoardManager](https://api.github.com/repos/CalinRadoni/ESP32BoardManager) *Basic board manager with WiFi AP / STA mode, HTTP server and firmware OTA update*<br/>C++
+- [ESP32DLEDController](https://api.github.com/repos/CalinRadoni/ESP32DLEDController) *ESP32 controller for WS2812 LEDs*<br/>C++, 3 stars, one fork
+- [ESP32HAL](https://api.github.com/repos/CalinRadoni/ESP32HAL) *ESP32 HAL: helper C++ classes for some ESP32 generic peripherals (ADC, GPIO, I2C, SPI, ...)*<br/>C++
+- [ESP32RMT](https://api.github.com/repos/CalinRadoni/ESP32RMT) *Helper component for ESP32 Remote Control module*<br/>C++, one fork
+- [ESP32SimpleOTA](https://api.github.com/repos/CalinRadoni/ESP32SimpleOTA) *Helper component for ESP32 Over The Air firmware updates*<br/>C++
+- [ESP32Timers](https://api.github.com/repos/CalinRadoni/ESP32Timers) *Helper component for ESP32 Timers*<br/>C++
+
 
 Not as much as I want, I am writing about various subjects at [CalinRadoni.github.io](https://github.com/CalinRadoni/CalinRadoni.github.io)
 
 At work I am doing mostly system engineering, network administration, engineering and security.
 
+## About this page
+
+This page is based on a jinja template filled by a python script.
+
 ## License
 
-If a license is needed, well ... use the [MIT License](LICENSE).
+This repository is licensed with the [MIT License](LICENSE).
---
+++ src/pageBuilder.py
@@ -0,0 +1,90 @@
+import sys
+import os
+from github import Github
+from jinja2 import Environment, FileSystemLoader
+
+
+class PageData:
+    """This class keeps and prepares the data to be rendered in page"""
+
+    def __init__(self, repos):
+        self.repos = repos
+
+    def get_repo(self, repoName):
+        """Returns the first repo with the name 'repoName' or None if not found."""
+        res = None
+        for repo in self.repos:
+            if repo.name == repoName:
+                res = repo
+                break
+        return res
+
+    def get_active_repo(self, repoName):
+        """Returns the first not archived repo with the name 'repoName' or None if not found."""
+        res = None
+        for repo in self.repos:
+            if not repo.archived:
+                if repo.name == repoName:
+                    res = repo
+                    break
+        return res
+
+    def get_active_repos_nb(self, repoNameStart):
+        """Returns all the not archived repos with the name starting with 'repoName' or None if not found."""
+        res = []
+        for repo in self.repos:
+            if not repo.archived:
+                if repo.name.startswith(repoNameStart):
+                    res.append(repo)
+        return res
+
+    def get_repo_namelink_md(self, repo, two_lines=False):
+        """Returns a markdown formatted string with the name and the description of the repo."""
+        s = "[" + repo.name + "](" + repo.url + ")"
+        if repo.description:
+            if two_lines:
+                s = s + "<br/>*" + repo.description + "*"
+            else:
+                s = s + " *" + repo.description + "*"
+        return s
+
+    def get_repo_info(self, repo):
+        """Returns a string with the language, the number of stars and the number of forks."""
+        s = repo.language
+        if repo.stargazers_count > 0:
+            if repo.stargazers_count == 1:
+                s = s + ", one star"
+            else:
+                s = s + ", " + str(repo.stargazers_count) + " stars"
+        if repo.forks_count > 0:
+            if repo.forks_count == 1:
+                s = s + ", one fork"
+            else:
+                s = s + ", " + str(repo.forks_count) + " forks"
+        return s
+
+    def create_output_data(self):
+        self.esp_components = self.get_active_repos_nb("ESP32")
+        return
+
+    def render(self, templateName):
+        fileLoader = FileSystemLoader(".")
+        env = Environment(loader=fileLoader)
+        template = env.get_template(templateName)
+        return template.render(info=self)
+
+
+if __name__ == "__main__":
+    token = os.getenv("GITHUB_TOKEN")
+    if not token:
+        print("GITHUB_TOKEN is not defined")
+        sys.exit(1)
+
+    g = Github(token)
+    repos = g.get_user().get_repos()
+
+    page_data = PageData(repos)
+    page_data.create_output_data()
+
+    with open("README.md", "w") as out_file:
+        out_file.write(page_data.render("src/README.md.j2"))
